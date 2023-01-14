89e8f30d5ee83007d0f410fe2841c9cdc5343276
Ken Mac <OxideO2CO2@gmail.com>
2023-01-14 15:57:30 +0800

    OKEH

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,15 +5,18 @@
     <label for="exampleInputEmail1">Identification Number:</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter ID number">
   </div>
+
   <div class="form-group">
     <label for="exampleFormControlTextarea1">Description:</label>
     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Describe Yourself"></textarea>
     <div class="form-check">
-  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-  <label class="form-check-label" for="exampleRadios1">
-    Default radio
-  </label>
 </div>
+<label for="exampleFormControlTextarea1">Status:</label>
+<select class="custom-select">
+  <option selected>Kindly Select your status</option>
+  <option value="Pend">Pending</option>
+  <option value="Comp">Completed</option>
+</select>
 
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
